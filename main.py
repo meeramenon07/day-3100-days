09f003072666a3d723c18deb416b1087365a5242
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-11-28 14:30:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#number = input("Give me a number: ")
+#group = input("Give me a collective noun for a group of things: ")
+#thing = input("Give me the name of a weird or wacky thing: ")
+#print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
+
+myFood = input("Name a food: ")
+myPlant = input("Name a type of plant : ")
+cookingMethod = input("Name a method of cooking: ")
+burnedFood = input("What word describes burned food? : ")
+diyItem = input("Name a DIY item : ")
+
+#myMenu = input("Menu \n Saute Banana & Cheese with ruined Curry Leaf on a bed of Banana Leaf \n Ingredients \n  Banana \n Curry Leaf \n Cheese \n Banana Leaf.")
+
+print("Here is my Recipe", myFood, "my favorite", myPlant, cookingMethod, burnedFood, "not sure of that ", diyItem," \n Menu \n Grilled Banana & Cheese with fresh curry leaf on a bed of ruined banana leaf." )
+
